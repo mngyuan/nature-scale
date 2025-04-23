02d60e3e1640e9310089b18bdcb615fd9cc3adca
Kevin Lee <hikevinmlee@gmail.com>
2025-04-23 17:35:17 +0100

    delete unused files

--- src/components/LogInSignUpForm.tsx
+++
@@ -1,8 +0,0 @@
-export default function LogInSignUpForm() {
-  return (
-    <>
-      <h2 className="p-8 text-3xl">Log In</h2>
-      <div id="buttonDiv" className="p-8 w-[248px]"></div>
-    </>
-  );
-}
